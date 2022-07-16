b58aa61446624bd923a8029f56606c6ed4a5bc31
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2022-07-16 20:45:36 +0700

    project ecommercial jsp servlet

---
+++ src/servlet/AdminQuanLyDonHang.java
@@ -0,0 +1,73 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.OrderDetail;
+import database.OrderDao;
+
+/**
+ * Servlet implementation class AdminQuanLyDonHang
+ */
+@WebServlet("/AdminQuanLyDonHang")
+public class AdminQuanLyDonHang extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdminQuanLyDonHang() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		ArrayList<OrderDetail> list = null;
+		if (action.equalsIgnoreCase("showList")) {
+			try {
+				list = new OrderDao().getListOrderDetail();
+				request.setAttribute("list", list);
+				request.getRequestDispatcher("/admin/quanlydonhang.jsp").forward(request, response);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else if (action.equalsIgnoreCase("delete")) {
+			String OrderDetailID = request.getParameter("OrderDetailID");
+			String OrderID = request.getParameter("OrderID");
+			try {
+				new OrderDao().deleteOrderDetailByOrderDetailID(Integer.parseInt(OrderDetailID),
+						Integer.parseInt(OrderID));
+				list = new OrderDao().getListOrderDetail();
+				request.setAttribute("list", list);
+				request.getRequestDispatcher("/admin/quanlydonhang.jsp").forward(request, response);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
